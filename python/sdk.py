934e9808d1dc322242ae7baf2a4d965cc14ca9bd
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-04-18 15:03:52 +0700

    add example resp

--- python/sdk.py
+++ python/sdk.py
@@ -1,47 +1,6 @@
 import json
 import requests
 import os 
-ApiKey = {
-    "key": str,
-    "project": str
-}
-
-Account = {
-    "username": str,
-    "password": str,
-    "project": str 
-}
-
-Address = {
-    "public_ip": str,
-    "private_ip": str
-}
-
-Secret = {
-    'edge_functions' : {
-        'user_keygen': '',
-        'proxy_register': '',
-        'session_authenticate': '',
-        'signaling_authenticate': '',
-        'turn_register': '',
-        'worker_profile_fetch': '',
-        'worker_register': '',
-        'worker_session_create': '',
-        'worker_session_deactivate': '',
-    },
-    'secret': {
-        'anon': '',
-        'url': ''
-    },
-    'google': {
-        'client_id': ''
-    },
-    'conductor': {
-        'host': '',
-        'grpc_port': ''
-    }
-}
-
 
 class SdkFunction:
     def __init__(self):
